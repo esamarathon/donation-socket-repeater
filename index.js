4626779b32fa95adbfba9850d597be2cfb13842b
zoton2 <zoton2@gmail.com>
2018-07-02 15:11:47 +0100

    Add ability to receive/store/emit run info from NodeCG

--- index.js
+++ index.js
@@ -20,13 +20,25 @@ app.use(bodyParser.json());
 server.listen(port);
 console.log(`Listening on port ${port}.`);
 
+// Storage for stream information to send to clients.
+var streamInfo = {
+	stream1: null,
+	stream2: null
+}
+
+// Emit stream information to clients when they connect.
+io.on('connection', (socket) => {
+	console.log('Client connected with ID %s', socket.id);
+	socket.emit('streamInfo', streamInfo);
+});
+
 // A GET in case you need to check the server is running.
 app.get('/', (req, res) => {
 	res.send('Running OK');
 });
 
-// This is where the postbacks are received.
-app.post('/', (req, res) => {
+// This is where the tracker postbacks are received.
+app.post('/tracker', (req, res) => {
 	// Reject POSTs without the correct key.
 	if (req.query.key !== key) {
 		res.sendStatus(403);
@@ -71,4 +83,28 @@ app.post('/', (req, res) => {
 	}
 	
 	res.sendStatus(200);
+});
+
+// POSTS to here from streams on what their current run is.
+app.post('/stream_info', (req, res) => {
+	// Reject POSTs without the correct key.
+	if (req.query.key !== key) {
+		res.sendStatus(403);
+		return;
+	}
+	
+	// Store the data in the correct place.
+	if (req.body.stream === 1)
+		streamInfo.stream1 = req.body.runData;
+	else if (req.body.stream === 2)
+		streamInfo.stream2 = req.body.runData;
+
+	// Emit this information now that it's changed.
+	io.emit('streamInfo', streamInfo);
+	res.sendStatus(200);
+});
+
+// GETs to here return the stream information, if needed.
+app.get('/stream_info', (req, res) => {
+	res.json(streamInfo);
 });
